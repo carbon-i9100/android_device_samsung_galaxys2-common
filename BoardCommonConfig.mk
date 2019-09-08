dot-i9300/android_device_samsung_smdk4412-common

galaxys2-common: Add dummy keydisabler to allow navbar disabling

BoardCommonConfig.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+DEVICE_PATH := device/samsung/smdk4412-common
+
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 USE_CAMERA_STUB := false
@@ -117,7 +119,6 @@ TARGET_USES_LOGD := false
 
 # RIL
 BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
-# RIL
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6262
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/smdk4412-common/include
@@ -137,6 +138,9 @@ WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
 WIFI_BAND                        := 802_11_ABG
 BOARD_HAVE_SAMSUNG_WIFI          := true
+
+# Key disabler
+JAVA_SOURCE_OVERLAYS := com.samsung.keydisabler|$(DEVICE_PATH)/keydisabler|**/*.java
 
 # Network Routing
 TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE := true

carbon-i9100/android_device_samsung_galaxys2-common

Change-Id: I286672eb3525ed8c1e88b3ae65a0a68d5d688971

BoardCommonConfig.mk
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 
+DEVICE_PATH := device/samsung/galaxys2-common
+
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 BOARD_USES_GENERIC_AUDIO := false
@@ -112,6 +114,9 @@ BOARD_USE_YAMAHA_MC1N2_AUDIO := true
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6260
 BOARD_RIL_CLASS := ../../../device/samsung/galaxys2-common/ril
+
+# Key disabler
+JAVA_SOURCE_OVERLAYS := org.carbonrom.keydisabler|$(DEVICE_PATH)/keydisabler|**/*.java
 
 # Camera
 BOARD_CAMERA_HAVE_ISO := true